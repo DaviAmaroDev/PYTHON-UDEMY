82bd146e81e456b8da6b8507dc2b16a633e56602
DaviAmaroDev <davi15022011@gmail.com>
2026-07-15 12:22:39 -0300

    Update aula65.py

---
+++ curso/aula65.py
@@ -0,0 +1,13 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechos de código que podem ser reutilizados em diferentes partes do programa.
+Elas ajudam a organizar o código, tornando-o mais legível e fácil de manter.
+Em Python, as funções são definidas usando a palavra-chave `def`, seguida pelo nome da função e parênteses que podem conter parâmetros.
+"""
+
+def numero():
+    """
+    Função que retorna um número inteiro.
+    """
+    print(42)
+numero()  # Chamada da função, que imprime o número 42
